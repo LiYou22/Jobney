45dc178ab8a376b137f85cbbdc3d4fa52f1dd328
Yu Lai <yulai.linda@gmail.com>
2024-04-09 22:05:29 -0400

    implemented dashboard ui

--- src/JobneyMain.java
+++ src/JobneyMain.java
@@ -1,17 +1,33 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 import model.user.AdminUser;
 import view.UserTypeSelectionView;
 
 
 public class JobneyMain extends Application {
-    private AdminUser adminUser;
+    
+	private AdminUser adminUser;
 
     @Override
     public void start(Stage primaryStage) {
-        adminUser = new AdminUser("admin@example.com", "adminPassword");
-
-        UserTypeSelectionView userTypeSelectionView = new UserTypeSelectionView(primaryStage, adminUser);
+    	
+//        adminUser = new AdminUser("admin@example.com", "adminPassword");
+//
+//        UserTypeSelectionView userTypeSelectionView = new UserTypeSelectionView(primaryStage, adminUser);
+    	
+    	try {
+    		
+    		Parent root = FXMLLoader.load(getClass().getResource("view/DashboardUI.fxml"));
+    		Scene scene = new Scene(root, 1260, 870);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+    	} catch(Exception e) {
+    		e.printStackTrace();
+    	}
     }
 
     public static void main(String[] args) {
