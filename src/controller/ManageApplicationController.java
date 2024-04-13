bf5d16c59ce3e48ec8f8d620b155a9914ae0755c
Yu Lai <yulai.linda@gmail.com>
2024-04-13 14:17:31 -0400

    implemented progress bar feature

--- src/controller/ManageApplicationController.java
+++ src/controller/ManageApplicationController.java
@@ -3,13 +3,17 @@ import java.awt.Desktop;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
+import javafx.beans.value.ChangeListener;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
@@ -18,6 +22,7 @@ import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import model.application.Application;
 import model.enums.APPLICATIONSTATUS;
 import model.enums.INDUSTRY;
@@ -31,6 +36,10 @@ public class ManageApplicationController {
 
     @FXML
     private Button btn_add_note;
+    
+
+    @FXML
+    private Button btn_save_note;
 
     @FXML
     private ComboBox<String> btn_status;
@@ -109,13 +118,35 @@ public class ManageApplicationController {
 		btn_status.setValue(status);
 
 		// display current progress, by default is 0%(1 checkbox = 20%)
+		progress_bar.setProgress(0.0);
+		double value = 0.0;
+		NumberFormat percentFormat = NumberFormat.getPercentInstance();
+		String default_percent = percentFormat.format(value);
+		progress_label.setText(default_percent);
 		
+		// create a listener for checkbox selection changes
+		ChangeListener<Boolean> listener = (obs, oldVal, newVal) -> {
+			double progress = 0;
+			if(do_research_checkbox.isSelected()) progress += 0.2; 
+			if(tailor_resume_checkbox.isSelected()) progress += 0.2;
+			if(tailore_cover_checkbox.isSelected()) progress += 0.2;
+			if(submit_checkbox.isSelected()) progress += 0.2;
+			if(connect_checkbox.isSelected()) progress += 0.2;
+			
+			progress_bar.setProgress(progress);
+			String percent = percentFormat.format(progress);
+			progress_label.setText(percent);
+		};
 		
-
+		// assign the listener to each checkbox
+		do_research_checkbox.selectedProperty().addListener(listener);
+		tailor_resume_checkbox.selectedProperty().addListener(listener);
+		tailore_cover_checkbox.selectedProperty().addListener(listener);
+		submit_checkbox.selectedProperty().addListener(listener);	
+		connect_checkbox.selectedProperty().addListener(listener);
         
 	}
 	
-
 
     @FXML
     void ChangeStatus(ActionEvent event) {
@@ -124,8 +155,23 @@ public class ManageApplicationController {
 
     @FXML
     void GoBack(ActionEvent event) {
+    	// go back to application table
+    	
+//        URL fileUrl1 = getClass().getResource("/view/ManageApplicationUI.fxml");
+//        FXMLLoader loader1 = new FXMLLoader(fileUrl1);
+//        ManageApplicationController controller1 = new ManageApplicationController(selectedApplication);  
+//        loader1.setController(controller1);
+//        Pane view1 = loader1.load();
+        
 
     }
+    
+
+    @FXML
+    void saveNote(ActionEvent event) {
+
+    }
+   
 
     @FXML
     void addCoverLetter(ActionEvent event) {
