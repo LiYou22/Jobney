84c357647764c3a56e9d054b1b1d339e88fda631
Yu Lai <yulai.linda@gmail.com>
2024-04-16 19:53:50 -0400

    modify back button

--- src/controller/ManageApplicationController.java
+++ src/controller/ManageApplicationController.java
@@ -200,7 +200,8 @@ public class ManageApplicationController {
             loader.setController(applicationController);
             Pane previousView = loader.load();
             
-            applicationController.getDashboardController().getMainPane().getChildren().setAll(previousView);
+//            applicationController.getDashboardController().getHbox().getChildren().remove(0);
+//            applicationController.getDashboardController().getHbox().getChildren().add(previousView);
 
             
         } catch (IOException e) {
